79c5dc44a2f460be6cc77fe77df5213a399c6a19
Otavio RL <otaviohlopess@gmail.com>
2022-10-05 15:04:55 -0300

    ajuste nos tamanhos

--- src/components/firstSection/avatar.js
+++ src/components/firstSection/avatar.js
@@ -1,6 +1,6 @@
 const Avatar = () => {
   return (
-    <div className="w-1/2 max-w-lg flex justify-center">
+    <div className="w-3/5 max-w-lg flex justify-center">
       <img className="rounded-full bg-roxo mt-14" alt='Meu avatar' src='https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairDreads01&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=Hoodie&clotheColor=Black&eyeType=Happy&eyebrowType=Default&mouthType=Smile&skinColor=Pale'
       />
     </div>
