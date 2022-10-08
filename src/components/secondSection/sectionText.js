2b99c3599e75742aa8d22338e159f76b5f2167eb
Otavio RL <otaviohlopess@gmail.com>
2022-10-08 10:50:43 -0300

    add shadows

--- src/components/secondSection/sectionText.js
+++ src/components/secondSection/sectionText.js
@@ -1,6 +1,6 @@
 const SectionText = () => {
   return (
-    <div className="bg-roxo pt-10 pb-32 md:pt-20 md:pb-44 lg:pt-32 lg:pb-44 xl:pt-44 xl:pb-56 px-7 lg:px-32 xl:px-20% 2xl:px-30% flex flex-col items-center">
+    <div className="bg-roxo pt-10 pb-32 md:pt-20 md:pb-44 lg:pt-32 lg:pb-44 xl:pt-44 xl:pb-56 px-7 lg:px-32 xl:px-20% 2xl:px-30% flex flex-col items-center shadow-lg">
       <h1 className="text-white text-center text-pxl sm:text-h6 md:text-h5 lg:text-h4 font-bold mb-4">Olá, meu nome é Otavio</h1>
       <p className="text-white text-pmd sm:text-plg md:text-pxl lg:text-2xl text-center leading-7 sm:leading-8 md:leading-9 lg:leading-10">Desde pequeno eu sempre tive um grande interesse por computadores, e aos 8 anos aprendi a fazer um site básico usando html. Agora aos 20 anos estou tendo a incrível oportunidade de estudar na Trybe e me tornar um desenvolvedor web fullstack.</p>
   </div>
