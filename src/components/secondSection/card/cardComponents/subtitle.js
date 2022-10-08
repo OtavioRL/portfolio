2a286d22ffcc680b4f729349bae96842b5080a73
Otavio RL <otaviohlopess@gmail.com>
2022-10-08 10:48:09 -0300

    fix

--- src/components/secondSection/card/cardComponents/subtitle.js
+++ src/components/secondSection/card/cardComponents/subtitle.js
@@ -1,6 +1,6 @@
 const SubTitle = ({ subtitle }) => {
   return (
-    <h2 className='text-plg sm:text-pxl lg:text-plg text-center font-bold text-roxo mt-10'>{ subtitle }</h2>
+    <h2 className='text-plg sm:text-pxl lg:text-plg xl:text-pxl text-center font-bold text-roxo mt-10'>{ subtitle }</h2>
   );
 };
 
--- src/components/secondSection/card/index.js
+++ src/components/secondSection/card/index.js
@@ -4,7 +4,7 @@ import Back from './back';
 
 const Cards = () => {
   return (
-    <div className="p-10 lg:p-0 w-5/6 bg-slate-50 relative bottom-24 rounded-xl flex flex-col lg:flex-row items-center lg:items-start lg:justify-around lg:h-screen border-solid border-grey-100 border">
+    <div className="p-10 lg:p-0 w-5/6 bg-slate-50 relative bottom-24 rounded-xl flex flex-col lg:flex-row items-center lg:items-start lg:justify-around lg:h-screen xl:bottom-32 border-solid border-grey-100 border">
       <Design/>
       <Front/> 
       <Back/> 
